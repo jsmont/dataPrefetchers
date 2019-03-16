13a7cf51915ae73db1a7df14173a8cd9ba399212
jsmont <jsmont.sol@gmail.com>
2019-03-16 13:18:05 +0100

    Removing unneeded prints

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 REPOROOT=$(git rev-parse --show-toplevel)
-SIMULATOR_FOLDER=$REPOROOT/ChampSim
 TARGET_FOLDER=$REPOROOT/ChampSim/prefetcher
 BUILDER=$REPOROOT/ChampSim/build_champsim.sh
-
-cd $SIMULATOR_FOLDER
 
 BRANCH_PRED=perceptron
 LLC_REPLACEMENT=lru
@@ -28,15 +25,11 @@ echo "L2 prefetchers: ${L2_PREF[@]}"
 echo "LLC prefetchers: ${LLC_PREF[@]}"
 
 for l1p in "${L1_PREF[@]}"; do
-    echo $l1p
-done
-
-for l1p in "${L1_PREF[@]}"; do
     for l2p in "${L2_PREF[@]}"; do
         for llcp in "${LLC_PREF[@]}"; do
-            echo -e "Buidling l1:${l1p}\tl2:${l2p}\tllc:${llcp}"
-            $BUILDER ${BRANCH_PRED} ${l1p} ${l2p} ${llcp} ${LLC_REPLACEMENT} 1 > /dev/null
-            $BUILDER ${BRANCH_PRED} ${l1p} ${l2p} ${llcp} ${LLC_REPLACEMENT} 4 > /dev/null
+            echo "Buidling l1:${l1p}\tl2:${l2p}\tllc:${llcp}"
+            $BUILDER ${BRANCH_PRED} ${l1p} ${l2p} ${llcp} ${LLC_REPLACEMENT} 1
+            $BUILDER ${BRANCH_PRED} ${l1p} ${l2p} ${llcp} ${LLC_REPLACEMENT} 4
         done
     done
 done
