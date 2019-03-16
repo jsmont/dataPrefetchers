e4c14b4163041e7add816e64d5d8be7a0680197a
jsmont <jsmont.sol@gmail.com>
2019-03-16 16:58:59 +0100

    Changes to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 BASE_L1_PREF=$1
+BASE_L2_PREF=$2
+BASE_LLC_PREF=$3
+
 REPOROOT=$(git rev-parse --show-toplevel)
 SIMULATOR_FOLDER=$REPOROOT/ChampSim
 TARGET_FOLDER=$REPOROOT/ChampSim/prefetcher
@@ -22,25 +25,46 @@ function parsePrefList(){
 
 echo -e "Branch predictor: ${BRANCH_PRED}\tLLC replacement: ${LLC_REPLACEMENT}\tL1 prefetcher: ${BASE_L1_PREF}"
 
-if [ ! -f "$TARGET_FOLDER/$BASE_L1_PREF.l1d_pref" ]; then
-    echo "$BASE_L1_PREF does not exist!"
-    exit 1
-else 
-    #L1_PREF=($(parsePrefList l1d_pref))
-    L2_PREF=($(parsePrefList l2c_pref))
-    LLC_PREF=($(parsePrefList llc_pref))
+L1_PREF=($(parsePrefList l1d_pref))
+L2_PREF=($(parsePrefList l2c_pref))
+LLC_PREF=($(parsePrefList llc_pref))
 
-    #echo "L1 prefetchers: ${L1_PREF[@]}"
-    echo "L2 prefetchers: ${L2_PREF[@]}"
-    echo "LLC prefetchers: ${LLC_PREF[@]}"
+if [ -z "${$BASE_L1_PREF}" ]; then
+    L1_PREF=($BASE_L1_PREF)
+    if [ ! -f "$TARGET_FOLDER/$BASE_L1_PREF.l1d_pref" ]; then
+        echo "L1: $BASE_L1_PREF does not exist!"
+        exit 1
+    fi
+fi
+
+if [ -z "${$BASE_L2_PREF}" ]; then
+    L2_PREF=($BASE_L2_PREF)
+    if [ ! -f "$TARGET_FOLDER/$BASE_L2_PREF.l2c_pref" ]; then
+        echo "L2: $BASE_L2_PREF does not exist!"
+        exit 1
+    fi
+fi
+
+if [ -z "${$BASE_LLC_PREF}" ]; then
+    LLC_PREF=($BASE_LLC_PREF)
+    if [ ! -f "$TARGET_FOLDER/$BASE_LLC_PREF.llc_pref" ]; then
+        echo "LLC: $BASE_LLC_PREF does not exist!"
+        exit 1
+    fi
+fi
+
+echo "L1 prefetchers: ${L1_PREF[@]}"
+echo "L2 prefetchers: ${L2_PREF[@]}"
+echo "LLC prefetchers: ${LLC_PREF[@]}"
 
 
 
+for l1p in "${L1_PREF[@]}"; do
     for l2p in "${L2_PREF[@]}"; do
         for llcp in "${LLC_PREF[@]}"; do
             echo  -e "Buidling l1:${l1p}\tl2:${l2p}\tllc:${llcp}"
-            $BUILDER ${BRANCH_PRED} ${BASE_L1_PREF} ${l2p} ${llcp} ${LLC_REPLACEMENT} 1 > /dev/null
-            $BUILDER ${BRANCH_PRED} ${BASE_L1_PREF} ${l2p} ${llcp} ${LLC_REPLACEMENT} 4 > /dev/null
+            $BUILDER ${BRANCH_PRED} ${l1p} ${l2p} ${llcp} ${LLC_REPLACEMENT} 1 > /dev/null
+            $BUILDER ${BRANCH_PRED} ${l1p} ${l2p} ${llcp} ${LLC_REPLACEMENT} 4 > /dev/null
         done
     done
-fi
+done
